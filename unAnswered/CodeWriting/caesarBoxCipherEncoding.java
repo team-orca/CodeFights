1a880a04e9357edebfe1b75c3310bdc0198d6ffa
Ozan Onur TEK <ozantek@std.iyte.edu.tr>
2016-02-29 14:54:13 +0200

    Added unChecked solution

--- unAnswered/CodeWriting/caesarBoxCipherEncoding.java
+++ unAnswered/CodeWriting/caesarBoxCipherEncoding.java
@@ -1,2 +1,14 @@
 String caesarBoxCipherEncoding(String inputString) {
 }
+//Solution ( created on mobilephone check syntax before use it)
+String caesarBoxCipherEncoding(String inputString) {
+int length = inputString.length;
+String returningString="";
+int rows = (int)Math.sqrt (length);
+for (int i=0; i <length; i++){
+returningString += "" + inputString.charAt (i);
+if (i%rows==0 && i!=0)
+returningString +="\n";
+}
+return returningString;
+}
