2897e8415fcd2a480225e775f0b3698174e16ab8
Enes Günday <opsenes@gmail.com>
2016-02-29 15:48:22 +0200

    aaa
    Caesar Box is a simple transposition cipher used in the Roman Empire, in which characters of the given message are broken into multiple lines that form a square when stacked up and then read the square column by column.
    
    Given a message, encode it.
    
    Example
    
    For inputString = "a message", the square will look as follows:
    
    a m
    ess
    age
    
    Thus, the encoded message will be aea sgmse.
    
        [input] string inputString
            A string of length n2 for some integer n.
    
        [output] string

--- unAnswered/CodeWriting/caesarBoxCipherEncoding.java
+++ unAnswered/CodeWriting/caesarBoxCipherEncoding.java
@@ -12,3 +12,24 @@ returningString +="\n";
 }
 return returningString;
 }
+
+
+Caesar Box is a simple transposition cipher used in the Roman Empire, in which characters of the given message are broken into multiple lines that form a square when stacked up and then read the square column by column.
+
+Given a message, encode it.
+
+Example
+
+For inputString = "a message", the square will look as follows:
+
+a m
+ess
+age
+
+Thus, the encoded message will be aea sgmse.
+
+    [input] string inputString
+        A string of length n2 for some integer n.
+
+    [output] string
+
