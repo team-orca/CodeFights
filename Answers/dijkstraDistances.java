d27727499c5808da8d3e7efcd4a92ce2fafd2627
Ozan Onur TEK <ozantek@std.iyte.edu.tr>
2016-03-08 21:42:39 +0200

    Create dijkstraDistances.java

---
+++ Answers/dijkstraDistances.java
@@ -0,0 +1,26 @@
+int[] dijkstraDistances(int[][] matrix, int startVertex) {
+
+  boolean[] used = new boolean[matrix.length];
+  int[] distance = new int[matrix.length];
+  Arrays.fill(distance, Integer.MAX_VALUE);
+  distance[startVertex] = 0;
+
+  for (int i = 0; i < matrix.length; i++) {
+    int chosenIndex = -1;
+    for (int j = 0; j < matrix.length; j++) {
+      if (!used[j] && (chosenIndex == -1 ||
+          distance[chosenIndex] > distance[j])) {
+        chosenIndex = j;
+      }
+    }
+    used[chosenIndex] = true;
+    for (int j = 0; j < matrix.length; j++) {
+      if (matrix[chosenIndex][j] != -1 &&
+          distance[j] > distance[chosenIndex] + matrix[chosenIndex][j]) {
+        distance[j] = distance[chosenIndex] + matrix[chosenIndex][j];
+      }
+    }
+  }
+
+  return distance;
+}
