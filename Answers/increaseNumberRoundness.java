4c580c258d253e0b46664e40194d9924a04ac60f
Ozan Onur TEK <ozantek@std.iyte.edu.tr>
2016-03-15 01:44:04 +0200

    Create increaseNumberRoundness.java
    Define an integer's roundness as the number of trailing zeroes in it.
    
    Given an integer n, check if it's possible to increase n's roundness by swapping some pair of its digits.
    
    Example
    
    For n = 902200100, the output should be true (for instance, one may swap the leftmost 0 with 1).
    For n = 11000, the output should be false.
    [input] integer n
    
    A positive integer.
    [output] boolean
    
    true if it's possible to increase n's roundness, false otherwise.

---
+++ Answers/increaseNumberRoundness.java
@@ -0,0 +1,14 @@
+boolean increaseNumberRoundness(int n) {
+
+  boolean gotToSignificant = false;
+  while (n > 0) {
+    if (n % 10 == 0 && gotToSignificant) {
+      return true;
+    } else if (n % 10 != 0) {
+      gotToSignificant = true;
+    }
+    n /=  10 ;
+  }
+
+  return false;
+}
