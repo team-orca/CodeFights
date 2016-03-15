c459f418185cd212bea24b48bd74361f38e9beb3
Ozan Onur TEK <ozantek@std.iyte.edu.tr>
2016-03-15 15:08:55 +0200

    Update BigNumber.java

--- Challenges/BigNumber.java
+++ Challenges/BigNumber.java
@@ -1,14 +1,10 @@
-String BigNumber(String S1, String S2, String O) {
-    BigDecimal v1 = new BigDecimal(S1);
-    BigDecimal v2 = new BigDecimal(S2);
-   
+String BigNumber(String S, String A, String O) {
+    BigDecimal V = new BigDecimal(S);
+    BigDecimal v = new BigDecimal(A);
+  
     
-   v1 = ( O.charAt(0) == '+') ? v1.add(v2) : v1.subtract(v2);
-    String f= R(v1);
-    return f;
-}
-String R(BigDecimal v1){
-        DecimalFormat d = new DecimalFormat("0.###################################");
-        String f = d.format(v1.stripTrailingZeros());
-        return f;
+   V = ( O.charAt(0) == '+') ? V.add(v) : V.subtract(v);
+   DecimalFormat d = new DecimalFormat("0.###################################");
+   String f = d.format(V.stripTrailingZeros());
+   return f;
 }
