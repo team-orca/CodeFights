3db7eb4887435b60fc8a818e59a4953c72996827
Ozan Onur TEK <ozanonurtek@gmail.com>
2016-05-04 10:23:02 +0300

    Update checkTriangle.pl

--- PL/checkTriangle.pl
+++ PL/checkTriangle.pl
@@ -3,7 +3,9 @@ checkTriangle(X1,Y1,X2,Y2,X3,Y3)
 	:- distance(X1,Y1,X2,Y2,Z),
 		distance(X1,Y1,X2,Y2,K),
 			distance(X1,Y1,X2,Y2,T),
-		Z+K > T,
-			K+T > Z,
-				Z+T > K.
-		  
+		Z+K > T, Z-K < T,
+			K+T > Z, K-T< Z,
+				Z+T > K, Z-T<K,
+		not(X1-X2 = X3-X2),
+		not(Y1-Y2 = Y3-Y2).
+		  			
